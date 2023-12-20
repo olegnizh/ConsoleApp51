e4e443a80ba9589b993c81e28a8ac1fb283f1257
olegnizh <olegnizh@mail.ru>
2023-12-20 22:21:58 +0300

    Add project files.

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
