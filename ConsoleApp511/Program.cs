27bdacf0e394d2512a66072c3cd9cc7724f3ad43
olegnizh <olegnizh@mail.ru>
2023-12-22 19:19:50 +0300

    5.1.1

---
+++ ConsoleApp511/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp511
+{
+    class MainClass
+    {
+
+            static void ShowColor()
+            {
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                var color = Console.ReadLine();
+
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+
+                (string name, int age) user = ("Евгения", 27);
+
+                Console.WriteLine("Мое имя: {0}", user.name);
+                Console.WriteLine("Мой возраст: {0}", user.age);
+                
+                Console.Write("Введите имя: ");
+                user.name = Console.ReadLine();
+                Console.Write("Введите возрас с цифрами:");
+                user.age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ваше имя: {0}", user.name);
+                Console.WriteLine("Ваш возраст: {0}", user.age);
+
+                ShowColor();
+                
+
+            }
+
+
+
+
+
+    }
+}
