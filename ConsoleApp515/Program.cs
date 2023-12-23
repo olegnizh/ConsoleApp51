2d0d802e24abf7a1d65b250b514e0da6c9c84a99
olegnizh <olegnizh@mail.ru>
2023-12-23 23:51:05 +0300

    5.1.5

---
+++ ConsoleApp515/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp515
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] favcolors = new string[ 3 ];            
+
+            for ( int i = 0; i < 3; i++)
+                favcolors[i] = ShowColor();
+
+            Console.WriteLine("Ваши любимые цвета на английском с маленькой буквы");
+            foreach (var color in favcolors)
+                Console.WriteLine(color);
+                
+
+
+
+        }
+
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+
+
+
+    }
+}
