915dd02b4ce491f79812d73494f6afb686aad94a
olegnizh <olegnizh@mail.ru>
2023-12-24 00:56:59 +0300

    5.1.6

---
+++ ConsoleApp516/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp516
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = GetArrayFromConsole();
+
+            int temp;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Вы ввели числа ");
+            foreach (var index in result)
+                Console.Write(index.ToString() + " ");
+
+
+
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+    }
+}
