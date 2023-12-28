2bde6f7555cc1f2c8b07d0dfc701fd8c9e73db46
olegnizh <olegnizh@mail.ru>
2023-12-28 21:14:29 +0300

    5.2.15

---
+++ ConsoleApp5215/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp5215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var array = GetArrayFromConsole();
+
+            var sortedarray = SortArray(array);
+
+            foreach (var index in sortedarray)
+                Console.Write(index.ToString() + " ");
+
+        }
+
+        static int[] SortArray(int[] result)
+        {
+
+            int temp;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        static int[] GetArrayFromConsole(int num = 5)
+        {
+            var result = new int[num];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+
+    }
+}
