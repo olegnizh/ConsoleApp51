63eb94528390b542fb963da2b2c4581ea9e4b884
olegnizh <olegnizh@mail.ru>
2023-12-28 20:47:17 +0300

    5.2.11

---
+++ ConsoleApp5211/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp5211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите ваше имя");
+            string username = Console.ReadLine();
+
+            string[] favcolors = new string[ 3 ] { "cyan", "red", "green" };
+
+            ShowColors( username, favcolors );
+
+
+        }
+
+        static void ShowColors( string username, params string[] favcolors )
+        {
+
+            for ( int i =0; i < 3; i++)
+               Console.WriteLine(favcolors[ i ] + " ");
+
+        }
+
+
+    }
+}
