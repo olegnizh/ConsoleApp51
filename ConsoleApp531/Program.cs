f4d014ced8176f2707fd39cd99d30540ff66b30e
olegnizh <olegnizh@mail.ru>
2023-12-29 23:31:08 +0300

    5.3.1

---
+++ ConsoleApp531/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp531
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 5;
+            ChangeAge( age );
+            Console.WriteLine("Ваш возраст {0}", age);
+
+        }
+
+        static void ChangeAge(int age)
+        {
+            Console.WriteLine("Ваш возраст");
+            age = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+
+    }
+}
