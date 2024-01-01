84d6044d4f12a9618c40f95d5fcb7bf7fad86521
olegnizh <olegnizh@mail.ru>
2024-01-01 19:07:42 +0300

    5.3.4 - ChangeName - parameter by ref

---
+++ ConsoleApp534/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp534
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Олег";
+            ChangeName(ref name);
+            Console.WriteLine("Ваше имя {0}", name);
+
+        }
+
+        static void ChangeName(ref string name)
+        {
+            Console.WriteLine("Ваше имя");
+            name = Console.ReadLine();
+
+        }
+
+
+    }
+}
