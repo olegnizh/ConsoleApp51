002bbd8fd8aecd269dc4c029d7c6a3f50df4b6f1
olegnizh <olegnizh@mail.ru>
2024-01-01 18:50:42 +0300

    5.3.3 - parameters by ref

---
+++ ConsoleApp533/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp533
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 5;
+            ChangeAge( ref age);
+            Console.WriteLine("Ваш возраст {0}", age);
+
+        }
+
+        static void ChangeAge( ref int age )
+        {
+            Console.WriteLine("Ваш возраст");
+            age = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+
+    }
+}
