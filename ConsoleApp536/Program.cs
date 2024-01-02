d2f71b4e9fd9946bb31ad9b86d005f9367ce64ec
olegnizh <olegnizh@mail.ru>
2024-01-02 22:32:36 +0300

    5.3.6 - modofocator parameter-  in - prevent value

---
+++ ConsoleApp536/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp536
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			var arr = new int[] { 1, 2, 3 };
+			BigDataOperation(arr);
+
+			Console.WriteLine( arr[0] );
+
+		}
+
+		static void BigDataOperation( in int[] arr )
+		{
+			arr[ 0 ] = 4;
+		}
+
+
+
+	}
+
+}
