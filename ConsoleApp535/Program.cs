68322e9eb152c1c6646ade0e2e9e5df0aac022bb
olegnizh <olegnizh@mail.ru>
2024-01-02 22:20:51 +0300

    5.3.5 - parameter array  - change

---
+++ ConsoleApp535/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp535
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			var arr = new int[] { 1, 2, 3 };
+			BigDataOperation(arr);
+
+			Console.WriteLine(arr[0]);
+
+		}
+
+		static void BigDataOperation(int[] arr)
+		{
+			arr[0] = 4;
+		}
+
+
+
+	}
+
+}
