95b697b809ab1e43c95c0c7480751718eafe7dc2
olegnizh <olegnizh@mail.ru>
2024-01-02 22:45:46 +0300

    5.3.8

---
+++ ConsoleApp538/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp538
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nums = 6;
+            var result = GetArrayFromConsole( ref nums );
+
+            foreach (var index in result)
+                Console.Write(index.ToString() + " ");
+
+
+
+        }
+
+        static int[] GetArrayFromConsole(ref int nums)
+        {
+            var result = new int[ nums ];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[ i ] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+
+
+
+    }
+}
