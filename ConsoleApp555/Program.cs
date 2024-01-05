d232d3323c30853d0289fc48e0b9f7e42b027257
olegnizh <olegnizh@mail.ru>
2024-01-05 22:47:12 +0300

    5.5.5 int factorial

---
+++ ConsoleApp555/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp555
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            int x = Factorial(20);
+
+            Console.WriteLine("Получилось {0}", x);
+            Console.ReadKey();
+
+        }
+
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+
+
+
+    }
+}
