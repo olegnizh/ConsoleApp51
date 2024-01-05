742a9aa1b47fa8830a226db05eceb45dafb4737d
olegnizh <olegnizh@mail.ru>
2024-01-05 15:18:01 +0300

    5.4.3

---
+++ ConsoleApp543/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp543
+{
+    class Program
+    {
+		// xolostaya
+		int val = 8;
+
+		public static void Main(string[] args)
+		{
+
+			int val = 10;
+			Doubled(ref val);
+			Console.WriteLine(val);
+			Console.ReadKey();
+		}
+
+		static void Doubled(ref int val)
+		{
+			val *= 2;
+		}
+
+
+
+	}
+}
