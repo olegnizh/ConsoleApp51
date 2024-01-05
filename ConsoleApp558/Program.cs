756d5bb157e8c0699a28e2e059ac36a8784bd683
olegnizh <olegnizh@mail.ru>
2024-01-06 00:04:40 +0300

    5.5.8

---
+++ ConsoleApp558/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp558
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			int x = PowerUp(2, 5);
+            Console.WriteLine("Получилось {0}", x);
+            Console.ReadKey();
+        }
+
+		public static int PowerUp(int N, byte pow)
+		{
+			byte d = 1;
+			if (pow == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				if (pow == 1)
+				{
+					return N;
+				}
+				else
+				{
+					return N * PowerUp(N, --pow);
+				}
+			}
+		}
+
+
+	}
+}
