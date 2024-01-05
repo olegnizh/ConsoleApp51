748cfacef34b2735e9831a8810e1727df5edbd46
olegnizh <olegnizh@mail.ru>
2024-01-05 15:21:14 +0300

    5.4.4

---
+++ ConsoleApp544/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp544
+{
+    class Program
+    {
+		// xolostaya
+		int val = 8;
+
+		public static void Main(string[] args)
+		{
+
+			int val = 9;
+			{
+				val++;
+			}
+
+			Console.WriteLine(val);
+			Console.ReadKey();
+
+		} 
+	}
+}
