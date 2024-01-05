95c2b920035555f7d69448f028bf71c88ef701ec
olegnizh <olegnizh@mail.ru>
2024-01-05 21:50:08 +0300

    5.5.4  BackgroundColor

---
+++ ConsoleApp554/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp554
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadKey();
+
+        }
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.BackgroundColor = (ConsoleColor)deep;
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+
+        }
+
+
+    }
+}
