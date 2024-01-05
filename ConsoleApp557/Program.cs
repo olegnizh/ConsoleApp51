12347050c709728f4d5de1ef44a75c5790b5502a
olegnizh <olegnizh@mail.ru>
2024-01-05 22:54:58 +0300

    5.5.7 decimal factorial

---
+++ ConsoleApp557/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp557
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x = Factorial(20);
+
+            Console.WriteLine("Получилось {0}", x);
+            Console.ReadKey();
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+    }
+}
